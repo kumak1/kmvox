7c29ae2196cfe620311953817b11b46792534ced
kumak1 <makino@pepabo.com>
2021-09-27 00:03:00 +0900

    update sample script

--- example/synthesis_to_viocevox.sh
+++ example/synthesis_to_viocevox.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-echo "start: $(date '+%T')"
+START_TIME=$(date '+%s')
+
 curl -s -X POST "localhost:50021/audio_query?text=%E3%83%86%E3%82%B9%E3%83%88&speaker=1&is_kana=true" > query.json
-echo "get: $(date '+%T')"
-curl -s -X POST -H "Content-Type: application/json" -d @query.json "localhost:50021/synthesis?speaker=1" > audio.wav
-echo "end: $(date '+%T')"
+curl -s -H "Content-Type: application/json" -X POST -d @query.json "localhost:50021/synthesis?speaker=1" > audio.wav
+
+END_TIME=$(date '+%s')
+RUN_TIME=$((END_TIME - START_TIME))
+
+echo "end: $RUN_TIME sec."
